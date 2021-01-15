3580e3c4f44ea4838744a7e4f1756c05372919d8
aminography <amin.maxim@gmail.com>
2021-01-15 23:55:25 +0330

    Lowercase keys in RadixTree insertion

--- domain/src/main/java/com/aminography/domain/city/adapter/RadixTreeAdapter.kt
+++ domain/src/main/java/com/aminography/domain/city/adapter/RadixTreeAdapter.kt
@@ -2,12 +2,13 @@ package com.aminography.domain.city.adapter
 
 import com.aminography.domain.ds.RadixTree
 import com.aminography.model.City
+import java.util.*
 
 /**
  * @author aminography
  */
 class RadixTreeAdapter(private val tree: RadixTree<City>) : Inserter<City> {
     override fun insert(element: City) {
-        tree.insert(element.name, element)
+        tree.insert(element.name.toLowerCase(Locale.getDefault()), element)
     }
 }
