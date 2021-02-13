3af6163d53dae0d0f3c6f036ac82a61673d18efd
aminography <aminography@gmail.com>
2021-02-13 13:25:47 +0330

    Add Parcelable extensions for converting to base64 strings

---
+++ app/src/main/java/com/aminography/worldcities/ui/util/Base64Utils.kt
@@ -0,0 +1,13 @@
+package com.aminography.worldcities.ui.util
+
+import android.util.Base64
+
+/**
+ * @author aminography
+ */
+
+val ByteArray.encodeBase64: String
+    get() = Base64.encode(this, Base64.NO_WRAP).toString(charset("UTF-8"))
+
+val String.decodeBase64: ByteArray
+    get() = Base64.decode(this, Base64.NO_WRAP)
